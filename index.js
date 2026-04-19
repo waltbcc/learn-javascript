14e31d71b8bba4161a74f0be91a79da88ff1a846
waltbcc <waltbcc@gmail.com>
2026-04-19 20:53:29 -0300

    first app in js

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById("count-el").innerText = 3;
+
+let count = 0;
+
+console.log (count);
+
+// 1. Create a variable, myAge, and set its to your age
+
+// console.log (myAge);
+
+let myAge = 21;
+
+// 2. Log the myAge variable to the console
+
+console.log (myAge);
