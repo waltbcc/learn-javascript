b4c127967b3819470429f52afe29316d52a35e71
waltbcc <waltbcc@gmail.com>
2026-04-19 21:50:22 -0300

    Adding button

---
+++ build-a-passenger-counter-app/index.js
@@ -0,0 +1,20 @@
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'Arial', sans-serif;
+}
+
+h1 {
+    font-size: 32px;
+    color: #000000;
+}
+
+h2 {
+    font-size: 24px;
+}
