4dc33d691827a887cdb3b050b556e8c6ed2129c9
waltbcc <waltbcc@gmail.com>
2026-04-19 21:06:31 -0300

    Basic mathematical operations

---
+++ basic-math-operation.js
@@ -0,0 +1,17 @@
+// let firstBatch = 5;
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch;
+
+// console.log (count);
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+let myAge = 21;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+
+console.log (myDogAge);
