e41f7a498d7b367c8ef10451c3060622437c5fd1
waltbcc <waltbcc@gmail.com>
2026-04-19 22:17:28 -0300

    writing function

---
+++ function.js
@@ -0,0 +1,16 @@
+function printNum() {
+    console.log(42);
+}
+
+printNum();
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function myLogger (a, b, c){
+    let sum = a + b + c;
+    console.log(sum);
+}
+
+myLogger (lap1, lap2, lap3);
